05fb743f02edc4a51245ae81c3e827ba68e8318b
njxue <ngjxue@gmail.com>
2022-05-11 15:22:22 +0800

    added CommentForm.js

---
+++ src/components/CommentForm.js
@@ -0,0 +1,40 @@
+import { Card, Form, Button } from "react-bootstrap";
+import { ref } from "../utils/firebase";
+import { useAuth } from "../contexts/AuthContext";
+import { useRef, useState } from "react";
+import { serverTimestamp } from "firebase/database";
+
+function CommentForm(props) {
+  const commentsRef = ref
+    .child("threads")
+    .child(props.threadId)
+    .child("comments");
+  const commentRef = useRef();
+
+
+  const { currUser } = useAuth();
+
+  function handleSubmitComment() {
+    const comment = {
+      author: { displayName: currUser.displayName, uid: currUser.uid },
+      createdAt: new Date().toLocaleString(),
+      body: commentRef.current.value,
+    };
+    commentsRef.push(comment);
+  }
+
+  return (
+    <Form onSubmit={handleSubmitComment}>
+      <Form.Group>
+        <Form.Control
+          type="text"
+          placeholder="comment"
+          ref={commentRef}
+        ></Form.Control>
+        <Button type="submit">Submit</Button>
+      </Form.Group>
+    </Form>
+  );
+}
+
+export default CommentForm;
