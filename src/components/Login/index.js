b3e05fa42cf30dd1f47602676dafdd53550e56a2
njxue <ngjxue@gmail.com>
2022-05-17 14:45:22 +0800

    re-styled login page using chakra

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,11 +1,25 @@
-import { Form, Button, Alert } from "react-bootstrap";
+//import { Form, Button, Alert } from "react-bootstrap";
 import { useRef, useState } from "react";
-import Container from "../layout/Container";
-import Padder from "../layout/Padder";
+
 import classes from "../../static/Auth.module.css";
 import { useAuth } from "../../contexts/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
-import Loader from "../layout/Loader";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  Alert,
+  AlertTitle,
+  Button,
+  AlertIcon,
+  Heading,
+  Stack,
+  Container,
+  Center,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
 
 function Login(props) {
   const emailRef = useRef();
@@ -13,19 +27,31 @@ function Login(props) {
 
   const { login } = useAuth();
   const [error, setError] = useState("");
+  const [missingEmail, setMissingEmail] = useState(false);
+  const [missingPassword, setMissingPassword] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
   async function handleSubmit() {
-    setError("");
-    setIsLoading(false);
+    setIsLoading(true);
+    setMissingEmail(false);
+    setMissingPassword(false);
+
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    if (email == "") {
+      setMissingEmail(true);
+    }
+
+    if (password == "") {
+      setMissingPassword(true);
+    }
 
     try {
-      setError("");
       setIsLoading(true);
-      await login(emailRef.current.value, passwordRef.current.value);
+      await login(email, password);
       navigate("/");
-    
     } catch {
       setError("Failed to Login");
     }
@@ -33,46 +59,51 @@ function Login(props) {
   }
 
   return (
-    <Container>
-      <Loader hidden={!isLoading} />
-      <div className={classes.auth}>
-        <Padder>
-          {error && (
-            <div>
-              <Alert variant="danger">{error}</Alert>
-            </div>
-          )}
-          <Container>
-            <h1>Login</h1>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group className="mb-3" id="email">
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control type="email" ref={emailRef} required />
-              </Form.Group>
-              <Form.Group className="mb-3" id="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" ref={passwordRef} required />
-              </Form.Group>
-              <Button
-                className={classes.button}
-                disabled={isLoading}
-                onClick={handleSubmit}
-              >
-                Login
-              </Button>
-            </Form>
-            <Padder>
-              <div className={classes.links}>
-                Need an account? <Link to="/register">Register here!</Link>
-              </div>
-              <div className={classes.links}>
-                Forgot password? <Link to="/resetpassword">Reset password here</Link>
-              </div>
-            </Padder>
-          </Container>
-        </Padder>
-      </div>
-    </Container>
+    <>
+      {error && (
+        <Alert status="error">
+          <AlertIcon />
+          <AlertTitle>{error}</AlertTitle>
+        </Alert>
+      )}
+      <Wrap
+        justify="center"
+        align="center"
+        direction="column"
+ 
+      >
+        <Heading as="h1">Login</Heading>
+        <form onSubmit={handleSubmit} style={{ paddingTop: "10px" }}>
+          <FormControl isRequired isInvalid={missingEmail}>
+            <FormLabel>Email Address</FormLabel>
+            <Input type="email" ref={emailRef} />
+            <FormErrorMessage>Email is required</FormErrorMessage>
+          </FormControl>
+          <FormControl isRequired isInvalid={missingPassword}>
+            <FormLabel>Password</FormLabel>
+            <Input type="password" ref={passwordRef} />
+            <FormErrorMessage>Password is required</FormErrorMessage>
+          </FormControl>
+
+          <Button
+            colorScheme="red"
+            isLoading={isLoading}
+            isDisabled={isLoading}
+            onClick={handleSubmit}
+            marginTop={7}
+            width="100%"
+          >
+            Login
+          </Button>
+        </form>
+        <div className={classes.links}>
+          Need an account? <Link to="/register">Register here!</Link>
+        </div>
+        <div className={classes.links}>
+          Forgot password? <Link to="/resetpassword">Reset password here</Link>
+        </div>
+      </Wrap>
+    </>
   );
 }
 
