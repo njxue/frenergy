e425a005b36f2bb45a8fc1d7f4ff559600b5a455
njxue <ngjxue@gmail.com>
2022-05-22 17:50:50 +0800

    Debugged votes, again

--- src/components/ThreadPage/Votes.js
+++ src/components/ThreadPage/Votes.js
@@ -6,13 +6,12 @@ import { HStack, IconButton } from "@chakra-ui/react";
 import { AiFillLike, AiOutlineLike } from "react-icons/ai";
 
 function Votes(props) {
-  //console.log("votes re-render");
   const { currUser } = useAuth();
   const { threadId, initialCount, module, category } = props;
 
   const [voteCount, setVoteCount] = useState(initialCount);
-  const [userHasUpvoted, setUserHasUpvoted] = useState(false);
-  const [isMounted, setIsMounted] = useState(false);
+  const [upvoted, setUpvoted] = useState();
+  //const [isMounted, setIsMounted] = useState(false);
 
   const upvotesRef = ref.child("upvotes").child(threadId).child(currUser.uid);
 
@@ -22,28 +21,13 @@ function Votes(props) {
     .child(threadId)
     .child("votes");
 
-  //listener that updates the votes and buttons. Only start listening after initial mount
-  useEffect(() => {
-    console.log("userHasUpvoted: " + userHasUpvoted);
-    if (isMounted) {
- 
-      const newVoteCount = userHasUpvoted ? voteCount + 1 : voteCount - 1;
-      setVoteCount(newVoteCount);
-      console.log("here: " + newVoteCount);
-      updateVotes(newVoteCount);
-    }
-  }, [userHasUpvoted]);
-
-  //on mount, set the initial button type and text using userHasUpvotes status
   useEffect(() => {
     upvotesRef.on("value", async (snapshot) => {
       const hasUpvoted = await snapshot.val();
       if (!snapshot.exists() || !hasUpvoted) {
-        setUserHasUpvoted(false);
- 
+        setUpvoted(false);
       } else {
-        setUserHasUpvoted(true);
- 
+        setUpvoted(true);
       }
     });
 
@@ -58,25 +42,28 @@ function Votes(props) {
     };
   }, []);
 
-  function updateVotes(newVoteCount) {
+  function handleClick() {
+    const newVoteCount = upvoted ? voteCount - 1 : voteCount + 1;
     const updateObject = {
       [`/posts/${module + category}/${threadId}/votes`]: newVoteCount,
       [`/postsByUsers/${currUser.uid}/${threadId}/votes`]: newVoteCount,
       [`/threads/${threadId}/post/votes`]: newVoteCount,
-      [`/upvotes/${threadId}/${currUser.uid}`]: userHasUpvoted,
+      [`/upvotes/${threadId}/${currUser.uid}`]: !upvoted,
     };
-    ref.update(updateObject);
+    ref.update(updateObject).then(() => {
+      setVoteCount(newVoteCount);
+      setUpvoted(!upvoted);
+    });
   }
 
-  return (
+  return upvoted == undefined ? (
+    <div></div>
+  ) : (
     <HStack>
       <IconButton
         size="xs"
-        as={userHasUpvoted ? AiFillLike : AiOutlineLike}
-        onClick={() => {
-          setIsMounted(true);
-          setUserHasUpvoted(!userHasUpvoted);
-        }}
+        as={upvoted ? AiFillLike : AiOutlineLike}
+        onClick={handleClick}
       />
       <div>{voteCount}</div>
     </HStack>
