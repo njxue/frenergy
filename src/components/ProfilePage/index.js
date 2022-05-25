2f70969c4c41d789aa7e570bc026e251fa18a532
njxue <ngjxue@gmail.com>
2022-05-25 16:51:23 +0800

    User's posts now appear in profile page

--- src/components/ProfilePage/index.js
+++ src/components/ProfilePage/index.js
@@ -1,15 +1,22 @@
 import SelectModules from "./SelectModules";
-import classes from "../../static/Profile.module.css";
+
 import ModulesList from "../Dashboard/ModulesList";
+import { Box, Flex, HStack } from "@chakra-ui/react";
+import UsersPosts from "./UserPosts";
+import { useAuth } from "../../contexts/AuthContext";
 
 function Profile() {
+  const { currUser } = useAuth();
   return (
-    <div>
-      <div className={classes.modules}>
+    <Flex justifyContent="center" flexWrap="wrap" gap={10} maxW="100vw">
+      <Flex direction="row" alignItems="top" flexWrap="wrap">
         <ModulesList editable={true} />
         <SelectModules />
-      </div>
-    </div>
+      </Flex>
+      <Box maxW="100vw">
+        <UsersPosts user={currUser} />
+      </Box>
+    </Flex>
   );
 }
 
