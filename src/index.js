93c5e05cc3dd5eb3c0f45bf00a48e60de40a3d03
njxue <ngjxue@gmail.com>
2022-05-25 22:37:24 +0800

    Changed import statement

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
