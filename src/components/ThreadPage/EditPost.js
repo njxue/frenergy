ca93588f2cef1ed26ef20ffc7cf30b249865b006
njxue <ngjxue@gmail.com>
2022-05-26 13:47:04 +0800

    Fixed title bug

--- src/components/ThreadPage/EditPost.js
+++ src/components/ThreadPage/EditPost.js
@@ -8,18 +8,31 @@ import {
 } from "@chakra-ui/react";
 
 import { useState } from "react";
-
+import { ref } from "../../config/firebase";
+import { useAuth } from "../../contexts/AuthContext";
 import SaveCancelButton from "../layout/SaveCancelButton";
 
 function EditPost(props) {
   const { post, setIsEditing, postRef } = props;
   const [title, setTitle] = useState(post.title);
   const [body, setBody] = useState(post.body);
+ 
+
+  const { currUser } = useAuth();
+  const userPostsRef = ref
+    .child("postsByUsers")
+    .child(currUser.uid)
+    .child(post.postId);
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(title);
+
     postRef.update({
+      title: title,
+      body: body,
+    });
+
+    userPostsRef.update({
       title: title,
       body: body,
     });
