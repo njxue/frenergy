2ad6f59b84bbfeac534532ba5751d840e28fe01e
njxue <ngjxue@gmail.com>
2022-05-27 19:48:28 +0800

    Added isLoading prop

--- src/components/layout/SaveCancelButton.js
+++ src/components/layout/SaveCancelButton.js
@@ -2,12 +2,12 @@ import { useDisclosure, Button, ButtonGroup } from "@chakra-ui/react";
 import ConfirmationModal from "./ConfirmationModal";
 
 function SaveCancelButton(props) {
-  const { action, actionOnConfirm } = props;
+  const { action, actionOnConfirm, isLoading } = props;
   const { onOpen, isOpen, onClose } = useDisclosure();
   return (
     <>
       <ButtonGroup>
-        <Button type="submit" size="sm" colorScheme="green">
+        <Button type="submit" size="sm" colorScheme="green" isLoading={isLoading}>
           Save
         </Button>
         <Button onClick={onOpen} size="sm" colorScheme="red">
