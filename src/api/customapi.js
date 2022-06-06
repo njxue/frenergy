bd0645f17a2c051403bc8c2fc6801e2ea7d39b39
njxue <ngjxue@gmail.com>
2022-06-06 15:14:04 +0800

    Add colors to major badges

---
+++ src/api/customapi.js
@@ -0,0 +1,141 @@
+export const CATEGORIES = ["General", "Exams", "Resources", "Off-topic"];
+export const BADGE_COLORS = {
+  "Chinese Language": "red",
+  "English Literature": "red",
+  "Japanese Studies": "red",
+  "South Asian Studies": "red",
+  "Chinese Studies": "red",
+  "Global Studies": "red",
+  "Malay Studies": "red",
+  "Southeast Asian Studies": "red",
+  "English Lang. & Linguistics": "red",
+  History: "red",
+  Philosophy: "red",
+  "Theatre & Perf. Studies": "red",
+
+  Anthropology: "red",
+  Geography: "red",
+  "Comms. and New Media": "red",
+  Economics: "red",
+  "Political Science": "red",
+  Psychology: "red",
+  "Social Work": "red",
+  Sociology: "red",
+
+  Chemistry: "red",
+  "Life Sciences": "red",
+  Physics: "red",
+  "Data Science and Analytics": "red",
+  Mathematics: "red",
+  "Quantitative Finance": "red",
+  "Food Science and Tech.": "red",
+  "Pharmaceutical Science": "red",
+  Statistics: "red",
+
+  Accountancy: "blue",
+  "Business Administration": "blue",
+  "Real Estate": "blue",
+
+  "Business Analytics": "black",
+  "Computer Science": "black",
+  "Information Security": "black",
+  "Information Systems": "black",
+  "Computer Engineering": "black",
+
+  Dentistry: "Purple",
+
+  Architecture: "yellow",
+  "Computer Engineering": "yellow",
+  Engineering: "yellow",
+  "Industrial Design": "yellow",
+  "Landscape Design": "yellow",
+
+  Law: "orange",
+
+  Medicine: "darkblue",
+
+  Nursing: "pink",
+
+  Pharmacy: "Green",
+
+  Music: "Brown",
+};
+
+
+export const FACULTIES = [
+  "CHS",
+  "Business",
+  "Computing",
+  "CDE",
+  "Dentistry",
+  "Law",
+  "Medicine",
+  "Pharmacy",
+  "Nursing",
+  "Music",
+];
+export const MAJORS = {
+  CHS: [
+    "Chinese Language",
+    "English Literature",
+    "Japanese Studies",
+    "South Asian Studies",
+    "Chinese Studies",
+    "Global Studies",
+    "Malay Studies",
+    "Southeast Asian Studies",
+    "English Lang. & Linguistics",
+    "History",
+    "Philosophy",
+    "Theatre & Perf. Studies",
+
+    "Anthropology",
+    "Geography",
+    "Comms. and New Media",
+    "Economics",
+    "Political Science",
+    "Psychology",
+    "Social Work",
+    "Sociology",
+
+    "Chemistry",
+    "Life Sciences",
+    "Physics",
+    "Data Science and Analytics",
+    "Mathematics",
+    "Quantitative Finance",
+    "Food Science and Tech.",
+    "Pharmaceutical Science",
+    "Statistics",
+  ],
+
+  Business: ["Accountancy", "Business Administration", "Real Estate"],
+
+  Computing: [
+    "Business Analytics",
+    "Computer Science",
+    "Information Security",
+    "Information Systems",
+    "Computer Engineering",
+  ],
+
+  Dentistry: ["Dentistry"],
+
+  CDE: [
+    "Architecture",
+    "Computer Engineering",
+    "Engineering",
+    "Industrial Design",
+    "Landscape Design",
+  ],
+
+  Law: ["Law"],
+
+  Medicine: ["Medicine"],
+
+  Nursing: ["Nursing"],
+
+  Pharmacy: ["Pharmacy"],
+
+  Music: ["Music"],
+};
---
+++ src/components/ProfilePage/MajorBadge.js
@@ -0,0 +1,14 @@
+import { Badge } from "@chakra-ui/react";
+import { BADGE_COLORS } from "../../api/customapi";
+
+function MajorBadge(props) {
+  const { major, size } = props;
+
+  return (
+    <Badge fontSize={size}  bg={BADGE_COLORS[major]} color="white">
+      {major}
+    </Badge>
+  );
+}
+
+export default MajorBadge;
