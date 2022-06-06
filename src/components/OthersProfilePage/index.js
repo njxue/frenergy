dc796d4e29cb285fdb39f6a8ccbed5b401f21977
njxue <ngjxue@gmail.com>
2022-06-06 16:50:25 +0800

    Changed styling

--- src/components/OthersProfilePage/index.js
+++ src/components/OthersProfilePage/index.js
@@ -9,8 +9,8 @@ import {
 } from "@chakra-ui/react";
 import { ref } from "../../config/firebase";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { useProfile } from "../../utils/helper";
+import { useNavigate, useParams } from "react-router-dom";
+
 import Loader from "../layout/Loader";
 import MajorBadge from "../ProfilePage/MajorBadge";
 import UserPosts from "../ProfilePage/UserPosts";
@@ -19,23 +19,29 @@ function UsersProfile() {
   const { username } = useParams();
   const [userData, setUserData] = useState();
   const [uid, setUid] = useState();
+  const navigate = useNavigate();
 
   useEffect(() => {
     ref
       .child("usernames")
       .child(username)
       .once("value", async (snapshot) => {
+        if (!snapshot.exists()) {
+          navigate("/dne");
+        }
         const data = await snapshot.val();
-        setUid(data)
+        setUid(data);
         ref
           .child("users")
           .child(data)
+          .child("profile")
           .once("value", (snapshot) => {
             setUserData(snapshot.val());
           });
       });
   }, [username]);
 
+  console.log(userData);
   return userData == undefined ? (
     <Loader />
   ) : (
