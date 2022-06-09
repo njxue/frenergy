09d189d8458ab046f64c5cdc9a1012cf03c4c875
njxue <ngjxue@gmail.com>
2022-06-09 15:59:42 +0800

    Changed styling

--- src/components/layout/Loader.js
+++ src/components/layout/Loader.js
@@ -1,12 +1,12 @@
-import { Spinner } from "@chakra-ui/react"
+import { Spinner, Box } from "@chakra-ui/react";
 import classes from "../../static/Loader.module.css";
 
 function Loader(props) {
   return (
-    <div className={classes.loader} hidden={props.hidden}>
+    <Box>
       <Spinner size="xl" />
       <h1>Loading...</h1>
-    </div>
+    </Box>
   );
 }
 
---
+++ src/components/layout/SkeletonLoader.js
@@ -0,0 +1,7 @@
+import { Skeleton } from "@chakra-ui/react";
+
+function SkeletonLoader() {
+  return <Skeleton w="100%" h="100%" />;
+}
+
+export default SkeletonLoader;
