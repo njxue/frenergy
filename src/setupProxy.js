55e39cac5261f6e9fed258372df96dfea8d0d7e7
njxue <ngjxue@gmail.com>
2022-06-21 16:18:32 +0800

    Set up proxy for NUSauth

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const proxy = require("http-proxy-middleware");
+const URL_PARAMS =
+  "response_type=code&client_id=INC000002524851&resource=sg_edu_nus_oauth&redirect_uri=https%3A%2F%2Ffrenergy.vercel.app%2F";
+module.exports = function (app) {
+  app.use(
+    proxy(`/adfs/oauth2/authorize?${URL_PARAMS}`, {
+      target: "https://vafs.nus.edu.sg",
+      changeOrigin: true,
+    })
+  );
+};
