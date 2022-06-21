f3b73839302c2461b55faa754a7f53fd76ccb6a5
njxue <ngjxue@gmail.com>
2022-06-21 16:18:05 +0800

    Added NUSauth

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -28,9 +28,9 @@ function AuthProvider(props) {
 
   function login(email, passsword) {
     const axios = require("axios").default;
-    console.log("oi");
+
     const url =
-      "https://vafs.nus.edu.sg/adfs/oauth2/authorize?response_type=code&client_id=INC000002524851&resource=sg_edu_nus_oauth&redirect_uri=https%3A%2F%2Ffrenergy.vercel.app%2F";
+      "/adfs/oauth2/authorize?response_type=code&client_id=INC000002524851&resource=sg_edu_nus_oauth&redirect_uri=https%3A%2F%2Ffrenergy.vercel.app%2F";
     axios
       .get(url)
       .then((response) => console.log(response))
