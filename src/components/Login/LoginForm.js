b15eba1a342a6eb63ac7eae492f7b57c8b935e77
njxue <ngjxue@gmail.com>
2022-06-24 18:57:14 +0800

    Change styling of Login Page

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,6 +1,5 @@
 import { useRef, useState } from "react";
-
-import classes from "../../static/Auth.module.css";
+import { AiOutlineLogin } from "react-icons/ai";
 import { useAuth } from "../../contexts/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 import {
@@ -9,23 +8,30 @@ import {
   FormErrorMessage,
   Input,
   Alert,
+  VStack,
   AlertTitle,
   Button,
   AlertIcon,
   Heading,
   Wrap,
+  Divider,
+  ButtonGroup,
+  HStack,
+  Text,
+  Image,
 } from "@chakra-ui/react";
-import { useError } from "../../utils/helper";
+import { useError, useWindowDimensions } from "../../utils/helper";
 function LoginForm() {
+  const navigate = useNavigate();
   const emailRef = useRef();
   const passwordRef = useRef();
 
   const { login } = useAuth();
   const { setError } = useError();
+  const { width } = useWindowDimensions();
   const [missingEmail, setMissingEmail] = useState(false);
   const [missingPassword, setMissingPassword] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const navigate = useNavigate();
 
   async function handleSubmit() {
     setIsLoading(true);
@@ -59,40 +65,50 @@ function LoginForm() {
   }
 
   return (
-    <>
-      <Wrap justify="center" align="center" direction="column">
+    <VStack align="stretch">
+      <HStack>
         <Heading as="h1">Login</Heading>
-        <form onSubmit={handleSubmit} style={{ paddingTop: "10px" }}>
-          <FormControl isRequired isInvalid={missingEmail}>
-            <FormLabel>Email Address</FormLabel>
-            <Input type="email" ref={emailRef} />
-            <FormErrorMessage>Email is required</FormErrorMessage>
-          </FormControl>
-          <FormControl isRequired isInvalid={missingPassword}>
-            <FormLabel>Password</FormLabel>
-            <Input type="password" ref={passwordRef} />
-            <FormErrorMessage>Password is required</FormErrorMessage>
-          </FormControl>
+        {width < 600 && (
+          <Image
+            boxSize="100px"
+            objectFit="cover"
+            src={require("../../static/logo.png")}
+          />
+        )}
+      </HStack>
 
+      <Divider h="1px" color="gray" />
+      <form onSubmit={handleSubmit} style={{ paddingTop: "10px" }}>
+        <FormControl isRequired isInvalid={missingEmail}>
+          <FormLabel>Email Address</FormLabel>
+          <Input bg="white" border="solid" type="email" ref={emailRef} />
+          <FormErrorMessage>Email is required</FormErrorMessage>
+        </FormControl>
+        <FormControl isRequired isInvalid={missingPassword}>
+          <FormLabel>Password</FormLabel>
+          <Input bg="white" border="solid" type="password" ref={passwordRef} />
+          <FormErrorMessage>Password is required</FormErrorMessage>
+        </FormControl>
+
+        <VStack align="stretch">
+          <Link to="/resetpassword">
+            <Text fontSize="xs">Forgot Password?</Text>
+          </Link>
           <Button
-            colorScheme="red"
+            colorScheme="teal"
             isLoading={isLoading}
             isDisabled={isLoading}
             onClick={handleSubmit}
             marginTop={7}
-            width="100%"
           >
             Login
           </Button>
-        </form>
-        <div className={classes.links}>
-          Need an account? <Link to="/register">Register here!</Link>
-        </div>
-        <div className={classes.links}>
-          Forgot password? <Link to="/resetpassword">Reset password here</Link>
-        </div>
-      </Wrap>
-    </>
+          <Button colorScheme="pink" onClick={() => navigate("/register")}>
+            Register
+          </Button>
+        </VStack>
+      </form>
+    </VStack>
   );
 }
 
