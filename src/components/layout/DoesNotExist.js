d73c7e4bebd8e4cb68b4b962c18d102797c75f1a
njxue <ngjxue@gmail.com>
2022-06-27 17:39:19 +0800

    Change styling

--- src/components/layout/DoesNotExist.js
+++ src/components/layout/DoesNotExist.js
@@ -1,5 +1,22 @@
-function DoesNotExist() {
-    return <div>This page does not exist</div>
+import { Heading, Text, Center, VStack, HStack, Image } from "@chakra-ui/react";
+
+function DoesNotExist(props) {
+  const { message } = props;
+  return (
+    <Center display="flex" justifyContent="center" h="inherit">
+      <VStack>
+        <HStack align="center">
+          <Heading size="2xl">404 :(</Heading>
+          <Image
+            boxSize="100px"
+            src={require("../../static/headtilt.png")}
+            objectFit="contain"
+          />
+        </HStack>
+        <Text>{message ? message : "Page was not found"}</Text>
+      </VStack>
+    </Center>
+  );
 }
 
 export default DoesNotExist;
