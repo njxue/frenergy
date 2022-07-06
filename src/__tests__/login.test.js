51d9b3922a0a35860f11c6bcb062a4b6814cf8c6
njxue <ngjxue@gmail.com>
2022-07-06 15:49:02 +0800

    Add render tests

--- src/__tests__/login.test.js
+++ src/__tests__/login.test.js
@@ -1,16 +1,105 @@
 import Login from "../components/Login";
 import LoginForm from "../components/Login/LoginForm";
-import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "../contexts/AuthContext";
-import { render, screen } from "@testing-library/react";
+import { render, screen, act, fireEvent } from "@testing-library/react/pure";
+import renderer from "react-test-renderer";
+import "@testing-library/jest-dom/extend-expect";
 
-it("Login fails if email or password input is empty", () => {
-  render(
-    <BrowserRouter>
-      <AuthProvider>
-        <LoginForm />
-      </AuthProvider>
-    </BrowserRouter>
-  );
+let component;
+beforeAll(
+  () =>
+    (component = render(
+      <BrowserRouter>
+        <AuthProvider>
+          <LoginForm />
+        </AuthProvider>
+      </BrowserRouter>
+    ))
+);
+
+it("Login renders", async () => {
+  // correct heading
+  const header = component.getByTestId("header");
+  expect(header.textContent).toBe("Login");
+
+  // form renders
+  const form = component.getByTestId("form");
+  expect(form).toBeInTheDocument();
+
+  // Login button renders
+  const loginBtn = component.getByTestId("login-btn");
+  expect(loginBtn.textContent).toBe("Login");
+
+  // Register button renders
+  const registerBtn = component.getByTestId("register-btn");
+  expect(registerBtn.textContent).toBe("Register");
+});
+
+it("Prevent login if email is empty", () => {
+  const emailInput = component.getByTestId("email-input");
+  const passwordInput = component.getByTestId("password-input");
+  const loginBtn = component.getByTestId("login-btn");
+
+  fireEvent.change(emailInput, {
+    target: {
+      value: "",
+    },
+  });
+
+  fireEvent.change(passwordInput, {
+    target: {
+      value: "password",
+    },
+  });
+
+  fireEvent.click(loginBtn);
+  const emailError = component.queryByTestId("email-error");
+  expect(emailError).not.toBeNull();
+});
+
+it("Prevent login if password is empty", () => {
+  const emailInput = component.getByTestId("email-input");
+  const passwordInput = component.getByTestId("password-input");
+  const loginBtn = component.getByTestId("login-btn");
+
+  fireEvent.change(emailInput, {
+    target: {
+      value: "email@email.com",
+    },
+  });
+
+  fireEvent.change(passwordInput, {
+    target: {
+      value: "",
+    },
+  });
+
+  fireEvent.click(loginBtn);
+  const passwordError = component.queryByTestId("password-error");
+  expect(passwordError).not.toBeNull();
+});
+
+it("No error message if email and password is provided", () => {
+  const emailInput = component.getByTestId("email-input");
+  const passwordInput = component.getByTestId("password-input");
+  const loginBtn = component.getByTestId("login-btn");
+
+  fireEvent.change(emailInput, {
+    target: {
+      value: "email@email.com",
+    },
+  });
+
+  fireEvent.change(passwordInput, {
+    target: {
+      value: "password",
+    },
+  });
+
+  fireEvent.click(loginBtn);
+  const emailError = component.queryByTestId("email-error");
+  const passwordError = component.queryByTestId("password-error");
+  expect(emailError).toBeNull();
+  expect(passwordError).toBeNull();
 });
--- src/__tests__/register.test.js
+++ src/__tests__/register.test.js
@@ -1,16 +1,31 @@
-import Register from "../components/Register";
-import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "../contexts/AuthContext";
+import { render, screen, act } from "@testing-library/react";
+import RegisterForm from "../components/Register/RegisterForm";
+import "@testing-library/jest-dom/extend-expect";
 
-it("renders properly", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(
+it("Register renders", async () => {
+  const component = render(
     <BrowserRouter>
       <AuthProvider>
-        <Register />
+        <RegisterForm />
       </AuthProvider>
-    </BrowserRouter>,
-    div
+    </BrowserRouter>
   );
+
+  // correct heading
+  const header = component.getByTestId("header");
+  expect(header.textContent).toBe("Register");
+
+  // form renders
+  const form = component.getByTestId("form");
+  expect(form).toBeInTheDocument();
+
+  // Login button renders
+  const loginBtn = component.getByTestId("login-btn");
+  expect(loginBtn.textContent).toBe("Login");
+
+  // Register button renders
+  const registerBtn = component.getByTestId("register-btn");
+  expect(registerBtn.textContent).toBe("Register");
 });
