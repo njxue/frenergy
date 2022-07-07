7d4bfe6da0663e3b0715e85349b27d63af8f2a73
njxue <ngjxue@gmail.com>
2022-07-07 17:13:01 +0800

    Fix bug where logged in user is able to view log in and registration page

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,8 +1,22 @@
- 
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 import PreLoginLayout from "../layout/PreLoginLayout";
 import LoginForm from "./LoginForm";
+import Loader from "../layout/Loader";
 
 function Login() {
+  const navigate = useNavigate();
+  const { loggedIn } = useAuth();
+
+  if (loggedIn == undefined) {
+    return <Loader />;
+  }
+
+  if (loggedIn) {
+    navigate("/");
+    return;
+  }
+
   return (
     <PreLoginLayout>
       <LoginForm />
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,7 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 import PreLoginLayout from "../layout/PreLoginLayout";
 import RegisterForm from "./RegisterForm";
+import Loader from "../layout/Loader";
 
 function Register() {
+  const navigate = useNavigate();
+  const { loggedIn } = useAuth();
+
+  if (loggedIn == undefined) {
+    return <Loader />;
+  }
+
+  if (loggedIn) {
+    navigate("/");
+    return;
+  }
   return (
     <PreLoginLayout>
       <RegisterForm />
