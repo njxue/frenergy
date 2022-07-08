c976fabbff2ee62c53ac2b0bf6a659c9a1d05632
njxue <ngjxue@gmail.com>
2022-07-08 14:24:09 +0800

    Remove imports

--- src/components/OthersProfilePage/index.js
+++ src/components/OthersProfilePage/index.js
@@ -1,25 +1,13 @@
-import {
-  VStack,
-  Text,
-  Avatar,
-  HStack,
-  Flex,
-  Box,
-  Heading,
-} from "@chakra-ui/react";
 import { ref } from "../../config/firebase";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 import Loader from "../layout/Loader";
-import UserAttributes from "../ProfilePage/UserAttributes";
-import UserPosts from "../ProfilePage/UserPosts";
-import UserModules from "../ProfilePage/UserModules";
-import ProfilePage from "../ProfilePage";
+import Profile from "../ProfilePage";
 
 function UsersProfile() {
   const { username } = useParams();
-  console.log(username);
+
   const [userData, setUserData] = useState();
   const [uid, setUid] = useState();
   const navigate = useNavigate();
@@ -47,7 +35,7 @@ function UsersProfile() {
   return userData == undefined || username == undefined ? (
     <Loader />
   ) : (
-    <ProfilePage uid={uid} />
+    <Profile uid={uid} />
   );
 }
 
