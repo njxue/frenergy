feefbd2b3f2209741272c4ebadd5571ea41f6526
njxue <ngjxue@gmail.com>
2022-07-08 14:22:25 +0800

    Remove import

--- src/components/CategoryPage/ThreadRow.js
+++ src/components/CategoryPage/ThreadRow.js
@@ -2,7 +2,6 @@ import { Tr, Td } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ref } from "../../config/firebase";
-import Loader from "../layout/Loader";
 import SkeletonLoader from "../layout/SkeletonLoader";
 
 function ThreadRow(props) {
