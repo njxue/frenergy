d29c4b16360970ca1f8ef11a986f28374b867421
njxue <ngjxue@gmail.com>
2022-07-18 17:07:35 +0800

    Break into smaller components to aid unit testing

---
+++ src/components/CategoryPage/ThreadDetails.js
@@ -0,0 +1,44 @@
+import { HStack, VStack, Text, Heading } from "@chakra-ui/react";
+import UserAvatar from "../layout/UserAvatar";
+import Votes from "../ThreadPage/Votes";
+import { ref } from "../../config/firebase";
+import parse from "html-react-parser";
+import { useNavigate } from "react-router-dom";
+
+function ThreadDetails(props) {
+  const { post } = props;
+  const navigate = useNavigate();
+
+  return (
+    <HStack
+      shadow="md"
+      borderWidth="1px"
+      padding={5}
+      spacing={5}
+      _hover={{ backgroundColor: "#EFEDED" }}
+      cursor="pointer"
+      onClick={() => {
+        navigate(`/${post.moduleCode}/${post.category}/${post.postId}`);
+      }}
+      data-testId="container"
+    >
+      <UserAvatar uid={post.author} size="xl" disableClick />
+      <VStack align="stretch" spacing={3} w="100%">
+        <HStack direction="row" justifyContent="space-between" w="100%">
+          <Heading size="md" noOfLines={2} data-testId="title">
+            {post.title}
+          </Heading>
+          <Votes votesRef={ref.child(`votes/${post.postId}`)} disabled />
+        </HStack>
+        <Text noOfLines={2} fontSize="xs" data-testId="body">
+          {parse(post.body)}
+        </Text>
+        <Text fontSize="10px" data-testId="createdAt">
+          {post.createdAt}
+        </Text>
+      </VStack>
+    </HStack>
+  );
+}
+
+export default ThreadDetails;
