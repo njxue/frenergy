dd7d24a5d228e8fb305d9fc8598e763a2d376958
njxue <ngjxue@gmail.com>
2022-07-22 15:56:02 +0800

    Refactor to use only one button component

--- src/components/Groups/AcceptTaskButton.js
+++ src/components/Groups/AcceptTaskButton.js
@@ -1,29 +1,47 @@
 import { Button } from "@chakra-ui/react";
 import { ref } from "../../config/firebase";
 import { useAuth } from "../../contexts/AuthContext";
+import { useError } from "../../utils/helper";
+import { useState } from "react";
 
 function AcceptTaskButton(props) {
   const { task } = props;
-  const { taskId, projectId } = task;
   const { currUser } = useAuth();
+  const { taskId, projectId } = task;
+  const { setError } = useError();
+
+  const [accepted, setAccepted] = useState(
+    task.assignees && task.assignees[currUser.uid]
+  );
+
   const taskRef = ref.child(`projects/${projectId}/tasks/incomplete/${taskId}`);
 
-  const hasAccepted = task.assignees && task.assignees[currUser.uid];
-
   function handleAccept() {
-    taskRef.child(`assignees/${currUser.uid}`).set(true);
+    setAccepted(true);
+    taskRef.child(`assignees/${currUser.uid}`).set(true, (err) => {
+      if (err) {
+        setError("Unable to accept task");
+        setAccepted(false);
+      }
+    });
   }
 
   function handleRetract() {
-    taskRef.child(`assignees/${currUser.uid}`).set(null);
+    setAccepted(false);
+    taskRef.child(`assignees/${currUser.uid}`).set(null, (err) => {
+      if (err) {
+        setError("Unable to retract task");
+        setAccepted(true);
+      }
+    });
   }
-  return hasAccepted ? (
-    <Button colorScheme="red" onClick={handleRetract}>
-      Retract
-    </Button>
-  ) : (
-    <Button colorScheme="green" onClick={handleAccept}>
-      I'll do it!
+  return (
+    <Button
+      colorScheme={accepted ? "red" : "green"}
+      onClick={accepted ? handleRetract : handleAccept}
+      data-testid="taskBtn"
+    >
+      {accepted ? "Retract" : "I'll do it!"}
     </Button>
   );
 }
