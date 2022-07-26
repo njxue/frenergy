06ab30257a58f8c239c253966e55914136553ed5
njxue <ngjxue@gmail.com>
2022-07-26 12:30:24 +0800

    Add loading state to Chats

--- src/components/Chat/ChatList.js
+++ src/components/Chat/ChatList.js
@@ -1,23 +1,15 @@
-import {
-  VStack,
-  FormControl,
-  Input,
-  HStack,
-  InputGroup,
-  InputLeftElement,
-  IconButton,
-} from "@chakra-ui/react";
-import { ChatIcon } from "@chakra-ui/icons";
-import { RiSendPlaneFill } from "react-icons/ri";
+import { VStack, Text, HStack, Center, Heading } from "@chakra-ui/react";
+
 import { useEffect, useState, useRef } from "react";
 import { ref } from "../../config/firebase";
 import ChatItem from "./ChatItem";
-import { useAuth } from "../../contexts/AuthContext";
+import BlockwaveLoad from "../layout/BlockwaveLoad";
+import EmptyPrompt from "../Dashboard/EmptyPrompt";
 
 function ChatList(props) {
   const { chatId } = props;
   const chatRef = ref.child(`chats/${chatId}`);
-  const [chats, setChats] = useState([]);
+  const [chats, setChats] = useState();
   const messagesEnd = useRef(null);
 
   useEffect(() => {
@@ -29,12 +21,12 @@ function ChatList(props) {
           tmp.push(chats[k]);
         }
       }
+      console.log(tmp.length);
       setChats(tmp);
     });
   }, [chatId]);
 
   useEffect(() => {
-    console.log("scrolling");
     scrollToBottom();
   }, [chats]);
 
@@ -52,9 +44,25 @@ function ChatList(props) {
       overflow="auto"
       padding={2}
     >
-      {chats.map((chat) => {
-        return <ChatItem chat={chat} />;
-      })}
+      {chats == undefined ? (
+        <Center h="100%" w="100%">
+          <HStack>
+            <Heading>Loading Chat...</Heading>
+            <BlockwaveLoad />
+          </HStack>
+        </Center>
+      ) : chats[0] ? (
+        chats.map((chat) => {
+          return <ChatItem chat={chat} key={chat.time} />;
+        })
+      ) : (
+        <Center h="100%" w="100%">
+          <EmptyPrompt
+            group="chat messages"
+            message="Say hi to your new friends!"
+          />
+        </Center>
+      )}
       <div ref={messagesEnd}></div>
     </VStack>
   );
